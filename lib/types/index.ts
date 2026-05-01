599b87cacd842cfd0cf5f4233c2411280bd8a53e
iLINE-iSERI <iline.iseri@gmail.com>
2026-05-01 20:32:48 +0900

    Update index.ts

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -78,6 +78,12 @@ export interface QnA {
   createdAt: Timestamp
 }
 
+export interface Category {
+  id: string
+  name: string
+  slug: string
+  order: number
+}
 export interface StudentGroup {
   id: string
   name: string
