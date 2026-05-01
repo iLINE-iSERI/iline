303469ec51191c155945a4b8aa725811d4256269
iLINE-iSERI <iline.iseri@gmail.com>
2026-05-01 21:40:11 +0900

    Update index.ts

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -6,7 +6,7 @@ export interface UserProfile {
   displayName: string
   role: 'student' | 'teacher' | 'admin'
   group?: string
-  name?: string
+  name: string
   totalPoints: number
   createdAt: Timestamp
 }
